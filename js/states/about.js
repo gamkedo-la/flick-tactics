c0d3ae542dfe810cff578206f21b8c9bdd50f73b
Gamkedo <chris@gamkedo.com>
2021-03-19 13:17:01 -0700

    Credits integrated as of last night

--- js/states/about.js
+++ js/states/about.js
@@ -1,19 +1,35 @@
 const ABOUT = 2;
 var about = [];
 
-function aboutSetup() {
-    htgdLogo = new Sprite(tr(vec2(gameWidth / 2, gameHeight - (gameHeight / 4))), new ImageObject("images/htgd.png"));
-    htgdLogo.transform.scale = vec2((sizeFactor / 4) / htgdLogo.imageObject.image.width, (sizeFactor / 4) / htgdLogo.imageObject.image.height);
+var creditsList = [
+"Bilal A. Cheema: Project lead, core gameplay, building and tile art, logo, palette, battle screen, mechs art, zoom functionality, pathfinding, map art, audio integration,",
+"(Bilal continued): dialog system, map editor, power meter, enemy AI, particles, save and load, Zareem and Guru body, Taja faces, assorted improvements and fixes",
+"Kyle Knutson: Concept art, body and faces for Hulu, Jonah, and generic soldier, additional Guru and Zareem faces, level design (Great Divide)",
+"Ryan Young: Level design (maps 4, 5, 6, 7, two faction)",
+"Roc Lee: World music, main menu music, UI sounds, action sounds",
+"Ashleigh M.: Zareem concept art and mock ups, stats screen flow, building and player count",
+"Justin Chin: Level design (tutorial map 1, map 2, map 3), writing (level 1 tutorial, character power, win, lose)",
+"Alan Zaring: Music loop for player CO",
+"Gonzalo Delgado: Rifle mech idle animation, case-sensitive fix",
+"Rutger McKenna: Soldier sprites with walk and fly animations",
+"Allan Regush: Help menu, button event support, skip to gameplay debug feature",
+"Jonathan Peterson: Audio on/off mute toggle buttons, additional case-sensitive fix",
+"H Trayford: Question mark animation, improved handling of undefined errors",
+"Philip Greene: Handle mech HP reaching zero"
+];
 
-    var fontStr = (18 * pixelSize).toString() + "px " + uiContext.fontFamily;
+function aboutSetup() {
+    //htgdLogo = new Sprite(tr(vec2(gameWidth / 2, gameHeight - (gameHeight / 4))), new ImageObject("images/htgd.png"));
+    //htgdLogo.transform.scale = vec2((sizeFactor / 4) / htgdLogo.imageObject.image.width, (sizeFactor / 4) / htgdLogo.imageObject.image.height);
+
+    var fontStr = (11 * pixelSize).toString() + "px " + uiContext.fontFamily;
 
     labelGroup = [];
-    labelGroup.push(new Label(tr(), "Developed by Members of HomeTeam GameDev", fontStr));
-    labelGroup.push(new Label(tr(), "", fontStr));
-    labelGroup.push(new Label(tr(), "Bilal A. Cheema: Team Lead", fontStr));
-    labelGroup.push(new Label(tr(), "", fontStr));
-    labelGroup.push(new Label(tr(), "YOUR NAME WOULD BE MENTIONED HERE", fontStr));
-    labelGroup.push(new Label(tr(), "IF YOU COMMIT TO THIS PROJECT JUST ONCE!", fontStr));
+    // labelGroup.push(new Label(tr(), "Developed by Members of HomeTeam GameDev (Outpost)", fontStr));
+
+    for(var i=0;i<creditsList.length;i++) {
+        labelGroup.push(new Label(tr(), creditsList[i], fontStr));
+    }
 
     about.push(new FlexGroup(tr(vec2(0, 20 * pixelSize), vec2(gameWidth, gameHeight / 2)),
         new SubState(tr(), labelGroup), false, vec2(0, 20), vec2(1, 7), true));
@@ -35,17 +51,17 @@ function aboutResize() {
     about[0].transform.position = vec2(0, 20 * pixelSize);
     about[0].transform.scale = vec2(gameWidth, gameHeight / 2);
 
-    htgdLogo.transform.position = vec2(gameWidth / 2, gameHeight - (gameHeight / 4));
-    htgdLogo.transform.scale = vec2((sizeFactor / 4) / htgdLogo.imageObject.image.width, (sizeFactor / 4) / htgdLogo.imageObject.image.height);
+    // htgdLogo.transform.position = vec2(gameWidth / 2, gameHeight - (gameHeight / 4));
+    // htgdLogo.transform.scale = vec2((sizeFactor / 4) / htgdLogo.imageObject.image.width, (sizeFactor / 4) / htgdLogo.imageObject.image.height);
 
-    aboutToStartButton.transform.position = vec2(gameWidth - (80 * pixelSize), gameHeight - (40 * pixelSize));
-    aboutToStartButton.transform.scale = vec2(80 * pixelSize, 40 * pixelSize);
+    aboutToStartButton.transform.position = vec2(gameWidth - (80 * pixelSize), gameHeight - (20 * pixelSize));
+    aboutToStartButton.transform.scale = vec2(80 * pixelSize, 20 * pixelSize);
 }
 
 function aboutDraw(deltaTime) {
     drawWorldMapBG("#221111EE");
-    drawPerspectiveUnitsBG(WAR_BUILDING, CITY_BUILDING, SUPPORT_MECH, BLUE_TEAM);
-    htgdLogo.drawSc();
+    // drawPerspectiveUnitsBG(WAR_BUILDING, CITY_BUILDING, SUPPORT_MECH, BLUE_TEAM);
+    //htgdLogo.drawSc();
 }
 
 function aboutUpdate(deltaTime) {
